dd69f26ef1880e5968aaa22dfb7af3dc57a113a8
BertheAdrien <berthe.adrien@yahoo.fr>
2025-04-07 08:39:29 +0200

    test auto deploy

--- resources/views/dashboard/admin.blade.php
+++ resources/views/dashboard/admin.blade.php
@@ -3,6 +3,10 @@
 @section('main')
     
     <h1>Dashboard Admin</h1>
+    <form action="{{ route('logout') }}" method="POST">
+        @csrf
+        <button type="submit" class="btn btn-danger">Logout</button>
+    </form>
     
 
 @endsection
