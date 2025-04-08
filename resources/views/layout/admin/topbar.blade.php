9d69e528d5b418b2f8dddc1e31f232e66bd0da97
BertheAdrien <berthe.adrien@yahoo.fr>
2025-04-08 11:57:29 +0200

    magnifique admin

--- resources/views/layout/admin/topbar.blade.php
+++ resources/views/layout/admin/topbar.blade.php
@@ -16,7 +16,7 @@
             <!-- ============================================================== -->
             <!-- Logo -->
             <!-- ============================================================== -->
-            <a class="navbar-brand" href="index.html">
+            <a class="navbar-brand" href="{{ route('dashboard.admin') }}">
               <!-- Logo icon -->
               <b class="logo-icon ps-2">
                 <!--You can put here icon as well // <i class="wi wi-sunset"></i> //-->
