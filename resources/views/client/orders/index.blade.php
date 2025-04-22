42c3fe143dd2ce9fb20838cae0393438b90bbc57
BertheAdrien <berthe.adrien@yahoo.fr>
2025-04-22 11:49:28 +0200

    stp

--- resources/views/client/orders/index.blade.php
+++ resources/views/client/orders/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('main')
     <div class="container mt-4">
-        <h2>Mes Commande en cours</h2>
+        <h2>Ma commande en cours</h2>
             <div class="row">
                     <div class="col-md-6 mb-4">
                         <div class="card">
