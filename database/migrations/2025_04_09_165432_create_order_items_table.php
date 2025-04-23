4e19d67613ca4789bea57b31cd50d542c377c507
BertheAdrien <berthe.adrien@yahoo.fr>
2025-04-23 13:49:08 +0200

    pb de delete

--- database/migrations/2025_04_09_165432_create_order_items_table.php
+++ database/migrations/2025_04_09_165432_create_order_items_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('order_items', function (Blueprint $table) {
             $table->id();
             $table->foreignId('order_id')->constrained()->onDelete('cascade');
-            $table->foreignId('item_id')->constrained();
+            $table->foreignId('item_id')->constrained()->nullOnDelete()->nullable();
             $table->string('name');
             $table->integer('quantity');
             $table->decimal('price', 10, 2);
