9def64db30655199d0968e2dbc90a34ab52b3a82
BertheAdrien <berthe.adrien@yahoo.fr>
2025-05-06 14:04:04 +0200

    modif topbar

--- resources/views/layout/client/topbar.blade.php
+++ resources/views/layout/client/topbar.blade.php
@@ -19,27 +19,21 @@
           <li class="nav-item active">
             <a href="{{ route('client.dashboard') }}" class="nav-link">Home</a>
           </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle pe-3" role="button" id="pages" data-bs-toggle="dropdown" aria-expanded="false">Pages</a>
-            <ul class="dropdown-menu border-0 p-3 rounded-0 shadow" aria-labelledby="pages">
-              <li><a href="#" class="dropdown-item">About Us </a></li>
-            </ul>
-          </li>
         </ul>
       </div>
       
       <div class="col-sm-8 col-lg-2 d-flex gap-5 align-items-center justify-content-center justify-content-sm-end">
         <ul class="d-flex justify-content-end list-unstyled m-0">
           <li>
-          <form action="{{ route('logout') }}" method="POST">
-            @csrf
-            <button type="submit" class="btn btn-danger">Logout</button>
-          </form>
-          </li>
-          <li>
             <a href="#" class="p-2 mx-1" data-bs-toggle="offcanvas" data-bs-target="#offcanvasCart" aria-controls="offcanvasCart">
               <svg width="24" height="24"><use xlink:href="#shopping-bag"></use></svg>
             </a>
+          </li>
+          <li>
+            <form action="{{ route('logout') }}" method="POST">
+              @csrf
+              <button type="submit" class="btn btn-danger">Logout</button>
+            </form>
           </li>
         </ul>
       </div>
