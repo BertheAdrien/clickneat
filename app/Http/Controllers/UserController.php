62d89273e1b00e0eaa189f856be747738b2ff9c2
BertheAdrien <berthe.adrien@yahoo.fr>
2025-06-09 15:13:03 +0200

    users fix

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -28,6 +29,9 @@ class UserController extends Controller
         $user = new User();
 
         $user->name = $request->get('name');
+        $user->email = $request->get('email');
+        $user->password = Hash::make($request->get('password'));
+        $user->role = $request->get('role');
         
         $user->save();
     
